d0235f5be749083fd79e587bbc1f915d88ccd283
prerana1821 <prerananawar1@gmail.com>
2021-04-25 00:01:05 +0530

    feat: Added Home Page

---
+++ src/Home/Home.js
@@ -0,0 +1,92 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Home.css";
+
+export const Home = () => {
+  const [img, setImg] = useState("img1");
+  return (
+    <div className='home'>
+      <div className='banner'>
+        {img === "img1" && (
+          <div className='sliding-img img1'>
+            <h2 className='slogan'>
+              preStore helps Swimmers and Triathletes swim better and achieve
+              their goals.
+            </h2>
+            <Link to='/products'>
+              <button className='btn-main btn'>Shop Now</button>
+            </Link>
+          </div>
+        )}
+        {img === "img2" && <div className='sliding-img img2'></div>}
+        <button
+          style={
+            img === "img1"
+              ? { backgroundColor: "#f50057" }
+              : { backgroundColor: "#fbcfe8" }
+          }
+          className='btn-img'
+          onClick={() => setImg("img1")}
+        ></button>
+        <button
+          style={
+            img === "img2"
+              ? { backgroundColor: "#f50057" }
+              : { backgroundColor: "#fbcfe8" }
+          }
+          className='btn-img'
+          onClick={() => setImg("img2")}
+        ></button>
+      </div>
+
+      <h2 className='center-txt'>Content</h2>
+      <div className='categories-home'>
+        <Link to='/'>
+          <div className='card-category cat-img1'>
+            <h3>View Latest Articles & Blogs</h3>
+          </div>
+        </Link>
+        <Link to='/courses'>
+          <div className='card-category cat-img2'>
+            <h3>View Courses</h3>
+          </div>
+        </Link>
+      </div>
+
+      <footer className='main-footer'>
+        <p>
+          © | 2021 | <span className='pink-txt'>preStore</span>
+        </p>
+        <p className='pink-txt'>preStore by preCodes</p>
+
+        <ul className='footer-list'>
+          <li>
+            <a className='links' href='mailto: prerananw1@gmail.com'>
+              <i className='far fa-2x fa-envelope'></i>
+            </a>
+          </li>
+
+          <li>
+            <a className='links' href='https://github.com/prerana1821'>
+              <i className='fab fa-2x fa-github'></i>
+            </a>
+          </li>
+
+          <li>
+            <a className='links' href='https://twitter.com/precodes18'>
+              <i className='fab fa-2x fa-twitter'></i>
+            </a>
+          </li>
+          <li>
+            <a
+              className='links'
+              href='https://www.linkedin.com/in/prerana-nawar/'
+            >
+              <i className='fab fa-2x fa-linkedin-in'></i>
+            </a>
+          </li>
+        </ul>
+      </footer>
+    </div>
+  );
+};
